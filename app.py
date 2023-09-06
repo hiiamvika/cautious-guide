300c42cc6d23ea59e03d48ac92baa495e702868f
Vika Krysheva <v.krysheva@gmail.com>
2023-09-06 14:43:25 +0700

    пукача

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Крышева Виктория Дмитриевна, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>веб-сервер на Flask</h1>
+
+        <footer>
+            &copy; Крышева В.Д.,ФБИ-12, 3 курс, 2023
+        </footer>
+         </body>
+</html>
+"""
+
