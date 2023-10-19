55e544d886b8ad5d717f77a7c5cee7d636759b6a
Vika Krysheva <v.krysheva@gmail.com>
2023-10-19 11:07:44 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -4,3 +4,26 @@ lab4 = Blueprint('lab4',__name__)
 @lab4.route("/lab4")
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route("/lab4/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'vika' and password == '123':
+        return render_template('success4.html', username = username)
+    else:
+        error = 'Неверные логин и/или пароль'
+    if username == '':
+        error = 'Введите логин'
+    if password == '':
+        error = 'Введите пароль'
+    return render_template('login.html', error=error, username = username, password = password)
+
+
+@lab4.route("/lab4/success4")
+def success4():
+    username = request.args.get('username')
+    return render_template('success4.html', username = username)
