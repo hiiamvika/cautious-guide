7a4961c21490584398e62475641120fbe4367262
Vika Krysheva <v.krysheva@gmail.com>
2023-10-28 04:43:50 +0700

    Завершение работы

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,redirect, url_for, render_template, request
+from flask import Blueprint,redirect, url_for, render_template, request, make_response
 lab4 = Blueprint('lab4',__name__)
 
 @lab4.route("/lab4")
@@ -27,3 +27,139 @@ def login():
 def success4():
     username = request.args.get('username')
     return render_template('success4.html', username = username)
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge(): 
+    msg = ''
+    if request.method =='GET':
+        return render_template('fridge.html', msg=msg)
+    
+    temperature = request.form.get('temperature')
+
+    if temperature == '':
+        msg ='Не задана температура'
+    else:
+        temperature = int(temperature)
+        if temperature < -12:
+            msg ='Не удалось установить температуру — слишком низкое значение'
+        elif temperature > -1:
+            msg = 'Не удалось установить температуру — слишком высокое значение'
+        elif (temperature >= -12) and (temperature <= -9):
+            msg = f'Установлена температура: {temperature}°С ❄️❄️❄️'
+        elif (temperature >= -8) and (temperature <= -5):
+            msg = f'Установлена температура: {temperature}°С ❄️❄️'
+        elif (temperature >= -4) and (temperature <= -1):
+            msg = f'Установлена температура: {temperature}°С ❄️'
+    return render_template('fridge.html', temperature=temperature, msg=msg)
+
+
+@lab4.route("/lab4/zerno", methods = ['GET', 'POST'])
+def zerno():
+    if request.method == 'GET':
+        return render_template('zerno.html')
+    price = 0
+    msg = ''
+    msg2 = ''
+    zerno = request.form.get('zerno')
+    weight = request.form.get('weight')
+
+    if weight == '':
+            msg = 'Не задан нужный вес'
+            return render_template('zerno.html',msg=msg)
+    
+    weight = int(weight)
+
+    if zerno == 'barley':
+            price = 12000 * weight
+    elif zerno == 'oats':
+            price = 8500 * weight
+    elif zerno == 'wheat':
+            price = 8700 * weight
+    else: 
+        zerno == 'rye'
+        price = 14000 * weight
+
+    if weight <= 0:
+            msg = 'Неверное значение'
+            return render_template('zerno.html',msg=msg)
+    elif weight > 500:
+            msg = 'Нужного объема зерна нет в наличии'
+            return render_template('zerno.html',msg=msg)
+    elif weight > 50:
+            price= price - (price * 10 / 100)
+            msg2 = 'Применится скидка 10% за большой объем'
+    return render_template('zernoOK.html', price=price, zerno=zerno, weight=weight, msg=msg, msg2 = msg2)
+
+
+@lab4.route("/lab4/zernoOK")
+def zernoOK():
+    return render_template('zernoOK.html')
+
+
+# @lab4.route('/lab4/cookies', methods=['GET', 'POST'])
+# def cookies():
+#     resp = make_response(render_template('cookies.html'))
+
+#     msg = ''
+#     color = request.form.get('color')
+#     b_color = request.form.get('background-color')
+#     f_size = request.form.get('font-size')
+#     if color == b_color:
+#          msg = 'Не может быть текст и фон одним цветом'
+#          return render_template('cookies.html', msg = msg)
+    
+#     if not (int(f_size) >= 5) and (int(f_size)<= 30):
+#         msg = 'Размер текста должен быть от 5px до 30px'
+#         return render_template('cookies.html', msg = msg)
+    
+#     if color and b_color and f_size:
+#         resp.set_cookie('color', color)
+#         resp.set_cookie('background-color', b_color)
+#         resp.set_cookie('font-size', f"{f_size}px")
+#     return resp
+
+
+
+
+@lab4.route("/lab4/cookies", methods=['GET', 'POST'])
+def cookies():
+    if request.method =='GET':
+        return render_template('cookies.html')
+    
+    color = request.form.get('color')
+    b_color = request.form.get('background-color') 
+    f_size = request.form.get('font-size')
+    msg = ''
+
+    if color == b_color:
+        msg = 'цвет текста не должен совпадать с цветом фона'
+        return render_template('cookies.html', msg = msg)
+    elif color != b_color:
+        color = color
+
+    if f_size == '':
+        msg = 'Задайте размер текста'
+        return render_template('cookies.html', msg = msg)
+    
+    if 5<= int(f_size) <= 30:
+        f_size = str(f_size) + 'px'
+    else:
+        msg = 'Размер текста должен быть от 5px до 30px'
+        return render_template('cookies.html', msg = msg)
+    
+    headers = {
+         'Set-Cookie': 'color=' + color + f_size + b_color + '; path=/',
+         'Set-Cookie': 'font-size=' + f_size + '; path=/',
+         'Set-Cookie': 'background-color=' + b_color + '; path=/',
+         'Location': '/lab4/cookies'
+    }
+    return '', 303, headers
+
+
+
+
+
+
+
+
+
