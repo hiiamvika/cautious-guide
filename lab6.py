62d41005a56c6c277bdee590d6e8ef95efbdd747
Vika Krysheva <v.krysheva@gmail.com>
2023-11-20 21:20:52 +0700

    лаба 6 начало

---
+++ lab6.py
@@ -0,0 +1,6 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint,redirect, url_for, render_template, request, session
+import psycopg2
+
+lab6 = Blueprint("lab6",__name__)
+
