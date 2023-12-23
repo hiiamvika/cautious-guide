a77a28efe401a215ef871ee4d304d22b7a37749e
Vika Krysheva <v.krysheva@gmail.com>
2023-12-23 12:25:36 +0700

    лаба 9

---
+++ lab9.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, render_template, redirect, url_for, abort, jsonify
+from datetime import datetime
+
+
+lab9 = Blueprint ("lab9", __name__)
+
+@lab9.route('/lab9/')
+def main1():
+    return render_template('/lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return render_template('lab9/404.html'), 404
+
+@lab9.route('/lab9/500')
+def server_error():
+    raise Exception("Произошла некоторая ошибка")
+
+@lab9.app_errorhandler(Exception)
+def er500(e):
+   return render_template('lab9/500.html'), 500
+
+@lab9.route('/lab9', methods=['GET', 'POST'])
+def main():
+    if request.method =='GET':
+        return render_template('lab9/index.html')
+    if request.method == 'POST':
+        recipient_name = request.form.get("recipient_name")
+        recipient_gender = request.form.get("recipient_gender")
+        sender_name = request.form.get("sender_name")
+    
+        return render_template('lab9/postcard.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
